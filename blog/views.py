e8efc197d7fe70e6382e387207524901468bd532
Code Master <codemaster08240328@gmail.com>
2018-12-22 16:33:43 +1000

    feature: render with db. Post model add. django admin add

--- blog/views.py
+++ blog/views.py
@@ -1,36 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        "author": 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'Blog Content 1',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        "author": 'Jhon Doe',
-        'title': 'Blog Post 2',
-        'content': 'Blog Content 2',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        "author": 'James King',
-        'title': 'Blog Post 3',
-        'content': 'Blog Content 3',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        "author": 'CoreyMS',
-        'title': 'Blog Post 4',
-        'content': 'Blog Content 4',
-        'date_posted': 'August 27, 2018'
-    },
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
